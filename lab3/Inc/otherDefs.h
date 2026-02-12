01b7e9923bec7dacbe6843ce752c4b8feac3a8c9
Timber Monson <timbermonson@gmail.com>
2026-02-12 09:07:47 -0700

    Backfill otherdefs name changes

--- lab3/Inc/otherDefs.h
+++ lab3/Inc/otherDefs.h
@@ -1,3 +1,3 @@
-void HAL_RCC_GPIOC_CLK_Enable(void);
-void HAL_RCC_GPIOA_CLK_Enable(void);
-void HAL_RCC_SYSCFG_CLK_Enable(void);
+void RCC_GPIOC_CLK_Enable(void);
+void RCC_GPIOA_CLK_Enable(void);
+void RCC_SYSCFG_CLK_Enable(void);
--- lab3/Src/otherDefs.c
+++ lab3/Src/otherDefs.c
@@ -2,17 +2,17 @@
 #include "stm32f0xx_hal.h"
 #include "otherDefs.h"
 
-void HAL_RCC_GPIOC_CLK_Enable(void)
+void RCC_GPIOC_CLK_Enable(void)
 {
   RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
 }
 
-void HAL_RCC_GPIOA_CLK_Enable(void)
+void RCC_GPIOA_CLK_Enable(void)
 {
   RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 }
 
-void HAL_RCC_SYSCFG_CLK_Enable(void)
+void RCC_SYSCFG_CLK_Enable(void)
 {
   RCC->APB2ENR |= RCC_APB2ENR_SYSCFGCOMPEN;
 }
